81a8bb29b14836a5b4fe03c41916594a1f688a30
Minxpat <it24101496@my.sliit.lk>
2025-09-20 12:20:34 +0530

    Updated product Entity file

--- src/main/java/com/loginproject/loginapp/Product.java
+++ src/main/java/com/loginproject/loginapp/Product.java
@@ -17,45 +17,85 @@ public class Product {
     private Long pro_id;
 
     @Column(name = "pro_name")
-    private String proName;
+    private String proName;   //product name
 
-    private String category;
-    private double price;
-    private double discount;
-    private int quantity;
-    private String imagePath;
+    private String category;  //product category
+    private double price;     //price of a product
+    private double discount;  //Discount for product
+    private int quantity;     //quantity available
+    private String description;   //product description
+    private String imagePath;    // link for the image
 
     public Product() {}
 
-    public Product(String proName, String category, double price,double discount, int quantity,String imagePath) {
+    public Product(String proName, String category, double price,double discount, int quantity,String description,String imagePath) {
         this.proName = proName;
         this.category = category;
         this.price = price;
         this.discount=discount;
         this.quantity = quantity;
+        this.description=description;
         this.imagePath = imagePath;
     }
 
-    public Long getPro_id() { return pro_id; }
-    public void setPro_id(Long pro_id) { this.pro_id = pro_id; }
+    public Long getPro_id() { 
+        return pro_id; 
+    }
 
-    public String getProName() { return proName; }
-    public void setProName(String proName) { this.proName = proName; }
+    public void setPro_id(Long pro_id) { 
+        this.pro_id = pro_id; 
+    }
 
-    public String getCategory() { return category; }
-    public void setCategory(String category) { this.category = category; }
+    public String getProName() { 
+        return proName; 
+    }
 
-    public double getDiscount() { return discount; }
-    public void setDiscount(double discount) { this.discount = discount; }
+    public void setProName(String proName) { 
+        this.proName = proName; 
+    }
 
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
+    public String getCategory() { 
+        return category; 
+    }
 
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
+    public void setCategory(String category) { 
+        this.category = category; 
+    }
 
-    public String getImagePath(){ return imagePath; }
-    public void setImagePath(String imagePath){ this.imagePath=imagePath; }
+    public double getDiscount() { 
+        return discount; 
+    }
+    public void setDiscount(double discount) { 
+        this.discount = discount; 
+    }
+    
+    public double getPrice() { 
+        return price; 
+    }
+    public void setPrice(double price) { 
+        this.price = price; 
+    }
+
+    public int getQuantity() { 
+        return quantity; 
+    }
+    public void setQuantity(int quantity) { 
+        this.quantity = quantity; 
+    }
+
+    public String getDescription() { 
+        return description; 
+    }
+    public void setDescription(String description) { 
+        this.description = description; 
+    }
+
+    public String getImagePath(){ 
+        return imagePath; 
+    }
+    public void setImagePath(String imagePath){ 
+        this.imagePath=imagePath; 
+    }
 }
 
 
